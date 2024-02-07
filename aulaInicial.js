d0ae46cf918331fe26398c36868f59db1dab0c64
Wesley Monteiro <wmonteiro873@gmail.com>
2024-02-07 15:27:43 -0300

    Primeira arquivo JS + arquivo .gitignore

---
+++ aulaInicial.js
@@ -0,0 +1,21 @@
+// comentário de linha
+
+/* 
+    Comentário
+    de
+    bloco
+*/
+
+let primeiraVariavel
+primeiraVariavel = 'Amontada Valley'
+console.log(primeiraVariavel)
+
+let a = 5, b = '3', c = 10
+console.log(a + b + c)
+console.log(a - b - c)
+console.log(a * b * c)
+console.log(a / b / c)
+
+const prompt = require('prompt-sync')()
+let nome = prompt('Informe seu nome: ')
+console.log(nome)
